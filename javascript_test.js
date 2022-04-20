869dc37697312f159d8cd284886ecc20fbc3bd7d
Iliya Jokic <ilija09@gmail.com>
2022-04-20 13:13:17 +1000

    commit to main

--- javascript_test.js
+++ javascript_test.js
@@ -1,5 +1,7 @@
-// creates a button that will open a new window
-function myFunction() { 
+// creates a button that will open a new window and then close it
+function myFunction() {
     var myWindow = window.open("", "MsgWindow", "width=200,height=100");
     myWindow.document.write("<p>This is 'MsgWindow'.</p>");
+    myWindow.document.write("<p>Click the 'X' to close me.</p>");
+    myWindow.document.close();
 }
