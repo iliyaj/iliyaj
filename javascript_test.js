8324fa24bec2c8b92a924eac1d111ced93a27e5b
Iliya Jokic <ilija09@gmail.com>
2023-03-14 17:29:41 +1100

    Deleted file

--- javascript_test.js
+++
@@ -1,5 +0,0 @@
-// creates a button that will open a new window
-function myFunction() { 
-    var myWindow = window.open("", "MsgWindow", "width=200,height=100");
-    myWindow.document.write("<p>This is 'MsgWindow'.</p>");
-}
